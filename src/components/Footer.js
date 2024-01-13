4060027f4adeb29e83cc3d57592328b492fdc4f7
uniquepraj86 <prajktaarote86@gmail.com>
2024-01-13 12:57:35 +0530

    footer changes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,16 +1,103 @@
-import React from 'react'
-
-const Footer = () => {
+import React from 'react';
+import { Link } from 'react-router-dom';
+function Footer() {
   return (
-    <div>Footer</div>
+   <>
+   <footer className='py-3'>
+     <div className="container-xxl">
+        <div className="row d-flex align-items-center">
+            <div className="col-5">
+                <div className="footer-top-data d-flex gap-30 align-items-center">
+                    <img src="images/newsletter.png" alt="newsletter" />
+                    <h2 className='mb-0 text-white'>Sign Up for Newsletter</h2>
+                </div>
+            </div>
+            <div className="col-7">
+                <div className="input-group">
+                    <input 
+                    type="text"
+                    className="from-control py-1"
+                    placeholder='Your Email Address...'
+                    aria-label="Your Email Address..."
+                    aria-describedby='basic-addon2'
+                    />
+                    <span className='input-group-text p-3' id='basic-addon2'>
+                        Subscribe
+                    </span>
+                </div>
+            </div>
+        </div>
+     </div>
+   </footer>
+   <footer className='py-4'>
+   <div className="container-xxl">
+    <div className="row">
+        <div className="col-4">
+            <h4 className='text-white mb-4'>Contact Us</h4>
+            <div className='footer-link d-flex flex-column'>
+             <div>
+                <address className='text-white fs-6'>
+                    Hno:277 Near vill chopal,<br/> Soniapat,Haryana <br/>
+                    PinCode:131103
+                </address>
+                <address>
+                    <a href="tel:+91 8329383044"
+                     className='mt-4 d-block mb-3'>+91 8329383044
+                    </a>
+                    <a href="mailto:prajktaarote86@gmail.com" 
+                    className='mt-4 d-block mb-3'>
+                        mailto:prajktaarote86@gmail.com
+                    </a>
+
+                </address>
+             </div>
+            </div>
+        </div>
+        <div className="col-3">
+            <h4 className='text-white mb-4'>Information</h4>
+            <div className='footer-link d-flex flex-column'>
+             <Link className="text-white py-2 mb-1">Privacy Policy </Link>
+             <Link className="text-white py-2 mb-1">Refund Policy</Link>
+             <Link className="text-white py-2 mb-1">Shipping Policy</Link>
+             <Link className="text-white py-2 mb-1">Terms & Condition</Link>
+             <Link className="text-white py-2 mb-1">Blogs</Link>
+            </div>
+        </div>
+        <div className="col-3">
+            <h4 className='text-white mb-4'>Account</h4>
+            <div className='footer-link d-flex flex-column'>
+             <Link className="text-white py-2 mb-1">About</Link>
+             <Link className="text-white py-2 mb-1">Faq</Link>
+             <Link className="text-white py-2 mb-1">Contact</Link>
+            </div>
+        </div>
+        <div className="col-2">
+            <h4 className='text-white mb-4'>Quick Link</h4>
+            <div className='footer-link d-flex flex-column'>
+             <Link className="text-white py-2 mb-1">Laptops</Link>
+             <Link className="text-white py-2 mb-1">Headphone</Link>
+             <Link className="text-white py-2 mb-1">Tablets</Link>
+             <Link className="text-white py-2 mb-1">Watch</Link>
+            </div>
+        </div>
+    </div>
+   </div>
+   </footer>
+   <footer className='py-4 '>
+     <div className="container-xxl">
+        <div className="row">
+            <div className="col-12">
+                <div className="text-center mb-0 text-white">
+                    &copy;{new Date().getFullYear()};
+                    Powered by Developered
+                </div>
+             </div>
+        </div>
+     </div>
+   </footer>
+   </>
   )
 }
 
 export default Footer
-
-
-
-
-
-
 
