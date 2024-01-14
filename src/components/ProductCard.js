4e3ec0d62823a7933ee516f312ae6ebda1b512cc
uniquepraj86 <prajktaarote86@gmail.com>
2024-01-14 23:01:19 +0530

    added special product

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -6,9 +6,22 @@ const ProductCard = () => {
   return (
     <>
       <div className="col-3">
-        <div className="product-card">
+        <div className="product-card position-relative">
+          <div className="wishlist-icon position-absolute">
+            <Link>
+            <img src="images/wish.svg" alt="wishlist" />
+            </Link>
+          </div>
+
           <div className="product-image">
-            <img src="images/watch.jpg" alt="product image"/>
+            <img src="images/watch.jpg"
+            className="img-fluid"
+             alt="product image" 
+             />
+            <img src="images/watch-1.avif"
+            className="fluid"
+             alt="product image" 
+             />
           </div>
           <div className="product-details">
             <h6 className="brand">Havels</h6>
@@ -17,6 +30,19 @@ const ProductCard = () => {
             </h5>
             <ReactStars count={5} size={24} value="3" activeColor="#ffd700" />
             <p className="price">$1000.00</p>
+          </div>
+          <div className="action-bar position-absolute">
+            <div className="d-flex flex-column gap-15">
+              <Link>
+                <img src="images/prodcompare.svg" alt="compare" />
+              </Link>
+              <Link>
+                <img src="images/view.svg" alt="view" />
+              </Link>
+              <Link>
+                <img src="images/add-cart.svg" alt="addcart" />
+              </Link>
+            </div>
           </div>
         </div>
       </div>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import Container from "../components/Container";
-
 import Marquee from "react-fast-marquee";
 import BlogCard from "./BlogCard";
 import ProductCard from "../components/ProductCard";
+import SpecialProduct from "../components/SpecialProduct";
 
 const Home = () => {
   return (
     <>
-
       <Container className="home-wrapper-1 py-5 ">
 
         <div className="container-xxl">
@@ -128,6 +127,7 @@ const Home = () => {
           </div>
         </div>
       </Container>
+
       <Container class1="home-wrapper-2 py-5">
         <div className="row">
           <div className="col-12">
@@ -192,6 +192,82 @@ const Home = () => {
           </div>
         </div>
       </Container>
+
+      <Container class1="featured-wrapper py-5 home-wrapper-2">
+        <div className="container-xxl">
+          <div className="row">
+            <div className="col-12">
+              <h3 className="section-heading">Featured Collection</h3>
+            </div>
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
+          </div>
+        </div>
+      </Container>
+
+      <Container class1="special-wrapper py-5 home-wrapper-2">
+        <div className="container-xxl">
+          <div className="row">
+            <div className="col-12">
+              <h3 className="section-heading">Special Products</h3>
+            </div>
+            <SpecialProduct />
+            <SpecialProduct />
+          </div>
+        </div>
+      </Container>
+
+      <Container class1="marque-wrapper home-wrapper-2 py-5">
+        <div className="row">
+          <div className="col-12">
+            <div className="marquee-inner-wrapper card-wrapper">
+              <Marquee className="d-flex">
+                <div className="mx-4 w-25">
+                  <img src="images/brand-01.png" alt="brand" />
+                </div>
+                <div className="mx-4 w-25">
+                  <img src="images/brand-02.png" alt="brand" />
+                </div>
+                <div className="mx-4 w-25">
+                  <img src="images/brand-03.png" alt="brand" />
+                </div>
+                <div className="mx-4 w-25">
+                  <img src="images/brand-04.png" alt="brand" />
+                </div>
+                <div className="mx-4 w-25">
+                  <img src="images/brand-05.png" alt="brand" />
+                </div>
+                <div className="mx-4 w-25">
+                  <img src="images/brand-06.png" alt="brand" />
+                </div>
+                <div className="mx-4 w-25">
+                  <img src="images/brand-07.png" alt="brand" />
+                </div>
+                <div className="mx-4 w-25">
+                  <img src="images/brand-08.png" alt="brand" />
+                </div>
+              </Marquee>
+            </div>
+          </div>
+        </div>
+      </Container>
+     
+      <Container class1="blog-wrapper py-5 home-wrapper-2">
+        <div className="container-xxl">
+          <div className="row">
+            <div className="col-12">
+              <h3 className="section-heading">Our Latest News </h3>
+            </div>
+            <BlogCard />
+            <BlogCard />
+            <BlogCard />
+            <BlogCard />
+          </div>
+        </div>
+      </Container>
+
       <Container class1="famous-wrapper py-5 home-wrapper-2">
         <div className="row">
           <div className="col-3">
@@ -251,110 +327,6 @@ const Home = () => {
                 <p className="text-dark">
                   From $699 or $116.58/mo. for 12 mo.*
                 </p>
-              </div>
-            </div>
-          </div>
-        </div>
-      </Container>
-      <Container class1="marque-wrapper home-wrapper-2 py-5">
-        <div className="row">
-          <div className="col-12">
-            <div className="marquee-inner-wrapper card-wrapper">
-              <Marquee className="d-flex">
-                <div className="mx-4 w-25">
-                  <img src="images/brand-01.png" alt="brand" />
-                </div>
-                <div className="mx-4 w-25">
-                  <img src="images/brand-02.png" alt="brand" />
-                </div>
-                <div className="mx-4 w-25">
-                  <img src="images/brand-03.png" alt="brand" />
-                </div>
-                <div className="mx-4 w-25">
-                  <img src="images/brand-04.png" alt="brand" />
-                </div>
-                <div className="mx-4 w-25">
-                  <img src="images/brand-05.png" alt="brand" />
-                </div>
-                <div className="mx-4 w-25">
-                  <img src="images/brand-06.png" alt="brand" />
-                </div>
-                <div className="mx-4 w-25">
-                  <img src="images/brand-07.png" alt="brand" />
-                </div>
-                <div className="mx-4 w-25">
-                  <img src="images/brand-08.png" alt="brand" />
-                </div>
-              </Marquee>
-            </div>
-          </div>
-        </div>
-      </Container>
-      <Container class1="product-wrapper py-5 home-wrapper-2">
-        <div className="container-xxl">
-          <div className="row">
-            <div className="col-12">
-              <h3 className="section-heading">Featured Collection</h3>
-            </div>
-            <ProductCard />
-            <ProductCard />
-            <ProductCard />
-            <ProductCard />
-          </div>
-        </div>
-      </Container>
-      <Container class1="blog-wrapper py-5 home-wrapper-2">
-        <div className="container-xxl">
-          <div className="row">
-            <div className="col-12">
-              <h3 className="section-heading">Our Latest News </h3>
-            </div>
-            <BlogCard />
-            <BlogCard />
-            <BlogCard />
-            <BlogCard />
-          </div>
-        </div>
-      </Container>
-
-      <Container class1="home-wrapper-2 py-5">
-        <div className="row">
-          <div className="col-12">
-            <div className="servies d-flex align-items-center justify-content-between">
-              <div>
-                <img src="images/service.png" alt="services" />
-                <div>
-                  <h6></h6>
-                  <p></p>
-                </div>
-              </div>
-              <div>
-                <img src="images/service-02.png" alt="services" />
-                <div>
-                  <h6></h6>
-                  <p></p>
-                </div>
-              </div>
-              <div>
-                <img src="images/service-03.png" alt="services" />
-                <div>
-                  <h6></h6>
-                  <p></p>
-                </div>
-              </div>
-              <div>
-                <img src="images/service-04.png" alt="services" />
-                <div>
-                  <h6></h6>
-                  <p></p>
-                </div>
-              </div>
-              <div>
-                <img src="images/service-05.png" alt="services" />
-                <div>
-                  <h6></h6>
-                  <p></p>
-                </div>
               </div>
             </div>
           </div>
