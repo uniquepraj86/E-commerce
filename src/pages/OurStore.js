6c9fc35cae43f4f81ddecbbbe6ccb74b48c83dde
uniquepraj86 <prajktaarote86@gmail.com>
2024-01-18 07:34:33 +0530

    added Meta

--- src/pages/OurStore.js
+++ src/pages/OurStore.js
@@ -1,9 +1,13 @@
 import React from 'react'
-import BreadCrumb from '../components/BreadCrumb'
+import BreadCrumb from '../components/BreadCrumb';
+import Meta from "../components/Meta"
 
 const OurStore = () => {
   return (
     <>
+    <Meta title={"Our Store"}/>
+
+  
       <BreadCrumb  title='Our Store'/>
     </>
   )
