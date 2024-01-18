2c0d1e5a3120b6de6e09731076e6f22b557d1243
uniquepraj86 <prajktaarote86@gmail.com>
2024-01-18 08:30:50 +0530

    added content our store

--- src/pages/OurStore.js
+++ src/pages/OurStore.js
@@ -1,16 +1,37 @@
-import React from 'react'
-import BreadCrumb from '../components/BreadCrumb';
-import Meta from "../components/Meta"
+import React from "react";
+import BreadCrumb from "../components/BreadCrumb";
+import Meta from "../components/Meta";
 
 const OurStore = () => {
   return (
     <>
-    <Meta title={"Our Store"}/>
+      <Meta title={"Our Store"} />
+      <BreadCrumb title="Our Store"/>
+      <div className="store-wrapper home-wrapper-2">
+        <div className="container-xxl">
+          <div className="row">
+            <div className="col-3">
+              <div className="filter-card mb-3">
+                  <h3 className="filter-title">Shop By Categories</h3>
+              </div>
+              <div className="filter-card mb-3">
+                  <h3 className="filter-title">Shop By Categories</h3>
+              </div>
+              <div className="filter-card mb-3">
+                  <h3 className="filter-title">Shop By Categories</h3>
+              </div>
+              <div className="filter-card mb-3">
+                  <h3 className="filter-title">Shop By Categories</h3>
+              </div>
+            </div>
+            <div className="col-9">
 
-  
-      <BreadCrumb  title='Our Store'/>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
 export default OurStore;
